25f330e786fbc0d2c33a4d8a10fbe8be88c61157
Azerino Yogananta Gatot Subroto <azerino25@gmail.com>
2018-12-21 15:42:12 +0700

    display input to console

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 console.log("Javascript DOM");
+
+const formPokemonName = document.getElementById("pokemon-guest") 
+
+const submitPokemon = eventNow => {
+    eventNow.preventDefault();
+
+    const inputPokemonName = document.getElementById("pokemon-name").value
+
+    const data = inputPokemonName
+
+    displayToConsole(data)
+}
+
+const displayToConsole = data =>{
+    console.log(data)
+}
+
+formPokemonName.addEventListener("submit", submitPokemon)
